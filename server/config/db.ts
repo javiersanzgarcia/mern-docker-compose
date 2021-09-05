fe98fe3a203eef20b79f6d3ed97800be319124a0
Pracker <pracker@Prackers-iMac.local>
2021-09-05 19:23:07 +0200

    Configuring docker image foor TS

--- server/config/db.ts
+++ server/config/db.ts
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URI_DOCKER, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
